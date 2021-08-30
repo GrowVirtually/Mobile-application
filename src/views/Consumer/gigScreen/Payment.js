e6421dea909bd800f367e32daf63577d2fb8a2d7
DDhanushka <dimuthudhanushka8@gmail.com>
2021-08-30 10:35:13 +0530

    install webview

--- src/views/Consumer/gigScreen/Payment.js
+++ src/views/Consumer/gigScreen/Payment.js
@@ -3,6 +3,7 @@ import React from "react";
 import {SafeAreaView, StyleSheet, Text, View} from "react-native";
 import AppHeader from "../../Common/AppHeader";
 import {WebView} from "react-native-webview";
+import StripeCheckout from "react-native-stripe-checkout-webview";
 
 const Payment = () => {
   const navigation = useNavigation();
@@ -13,7 +14,19 @@ const Payment = () => {
       <View>
         <AppHeader navigation={navigation} title="Payment" showBackButton={true} />
       </View>
-      <WebView source={{uri: "https://www.google.com"}} />
+      <StripeCheckout
+        stripePublicKey="pk_test_51JMm8aC6jbszMiH2kyfC7NkvZZdF18vmFExsw4vtOBrS5xBVWKbEcFYuggt7bmIsHROzechpHypuBjuqlio3Foka00kokLDcDC"
+        checkoutSessionInput={{
+          sessionId: "cs_test_mK66sMhXoyNiST3rRdlDSNLH64esLttAjyhKe4VvdRAXGFuA5sYprmKj",
+        }}
+        onSuccess={({checkoutSessionId}) => {
+          console.log(`Stripe checkout session succeeded. session id: ${checkoutSessionId}.`);
+        }}
+        onCancel={() => {
+          console.log(`Stripe checkout session cancelled.`);
+        }}
+      />
+      {/* <WebView source={{uri: "https://www.google.com"}} /> */}
     </>
   );
 };
