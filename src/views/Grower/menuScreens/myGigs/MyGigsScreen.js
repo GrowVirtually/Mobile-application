1029af0c4e6f1e944229173a43766aa56ba4282e
anjana-dodampe <ahmdodampe@hotmail.com>
2021-09-04 14:40:00 +0530

    Email, Authentication worked

--- src/views/Grower/menuScreens/myGigs/MyGigsScreen.js
+++ src/views/Grower/menuScreens/myGigs/MyGigsScreen.js
@@ -1,25 +1,26 @@
-/* eslint-disable arrow-body-style */
-import React from "react";
-import {ScrollView, TouchableOpacity, StyleSheet, View, SafeAreaView} from "react-native";
+// src/views/Grower/NewGig.js
 
+import React from 'react';
+import { StyleSheet, View, Text,StatusBar, TouchableOpacity  } from 'react-native';
 
-import * as Colors from "../../../../styles/abstracts/colors";
-import AppHeader from "../../../Common/AppHeader";
+import * as Colors from '../../../../styles/abstracts/colors';
+import  AppHeader  from '../../../Common/AppHeader';
 
-export const MyGigsScreen = ({navigation}) => {
-
-
+function MyGigsScreen ({navigation}) {
   return (
     <View style={styles.container}>
        <StatusBar backgroundColor={Colors.primary.color} />
        <AppHeader navigation={navigation} title="My Support" />
-      <Text style={styles.text}>Support Screen</Text>
+      <Text style={styles.text}>My Gig</Text>
      
    
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
- 
+  
+
 });
+
+export default MyGigsScreen;
