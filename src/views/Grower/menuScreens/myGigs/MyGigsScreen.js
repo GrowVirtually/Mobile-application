02d756163500b5bb2018a2c37db407a7307b81ea
anjana-dodampe <ahmdodampe@hotmail.com>
2021-09-04 14:14:35 +0530

    Fixed

--- src/views/Grower/menuScreens/myGigs/MyGigsScreen.js
+++ src/views/Grower/menuScreens/myGigs/MyGigsScreen.js
@@ -1,9 +1,7 @@
 /* eslint-disable arrow-body-style */
 import React from "react";
 import {ScrollView, TouchableOpacity, StyleSheet, View, SafeAreaView} from "react-native";
-import {Searchbar} from "react-native-paper";
-import {GrowerContent} from "./components/GrowerContent";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
 
 import * as Colors from "../../../../styles/abstracts/colors";
 import AppHeader from "../../../Common/AppHeader";
@@ -12,35 +10,16 @@ export const MyGigsScreen = ({navigation}) => {
 
 
   return (
-    <SafeAreaView>
-      <AppHeader navigation={navigation} title="Gigs" />
-      <View style={styles.searchbarContainer}>
-        <Searchbar style={styles.searchbarLeft} placeholder="Search" />
-        <TouchableOpacity style={styles.searchbarRight}>
-          <MaterialCommunityIcons name="tune" color="#fff" size={30} />
-        </TouchableOpacity>
-      </View>
-      <ScrollView>
-        <GrowerContent />
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+       <StatusBar backgroundColor={Colors.primary.color} />
+       <AppHeader navigation={navigation} title="My Support" />
+      <Text style={styles.text}>Support Screen</Text>
+     
+   
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  searchbarContainer: {
-    padding: 5,
-    flexDirection: "row",
-    backgroundColor: Colors.primary.color,
-  },
-  searchbarLeft: {
-    width: "80%",
-  },
-  searchbarRight: {
-    padding: 10,
-    alignSelf: "center",
-    alignItems: "center",
-    backgroundColor: Colors.primary.color,
-    width: "20%",
-  },
+ 
 });
