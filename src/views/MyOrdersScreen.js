2df18cbd2523dad3cf5fa8a00b370bc3eb480daa
DDhanushka <dimuthudhanushka8@gmail.com>
2021-09-17 13:46:57 +0530

    orders ui

--- src/views/MyOrdersScreen.js
+++ src/views/MyOrdersScreen.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import {StyleSheet, Text, View} from "react-native";
+import {ScrollView} from "react-native-gesture-handler";
 import AppHeader from "./Common/AppHeader";
+import Order from "./Common/Order";
 
 const MyOrdersScreen = ({navigation}) => {
   return (
     <>
       <AppHeader navigation={navigation} title="My Orders" />
-      <View>
-        <Text>my orders</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <Order
+            orderId="4442"
+            date="20-Jan-2021"
+            grower="John Doe"
+            price="4432.13"
+            isOrderCompleted={true}
+          />
+          <Order
+            orderId="2442"
+            date="20-Jan-2021"
+            grower="John Doe"
+            price="832.13"
+            isOrderCompleted={false}
+          />
+        </View>
+      </ScrollView>
     </>
   );
 };
 
 export default MyOrdersScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+});
