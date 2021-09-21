f1515ad0c6ff0f4bf036edb1df3285541369a5ad
anjana-dodampe <ahmdodampe@hotmail.com>
2021-09-22 02:34:12 +0530

    Finalised app for demo

--- src/views/Grower/menuScreens/support/SupportScreen.js
+++ src/views/Grower/menuScreens/support/SupportScreen.js
@@ -1,26 +1,56 @@
 // src/views/Grower/NewGig.js
 
-import React from 'react';
-import { StyleSheet, View, Text,StatusBar, TouchableOpacity  } from 'react-native';
+import React from "react";
+import {StyleSheet, View, Text, StatusBar, TouchableOpacity, Image} from "react-native";
 
-import * as Colors from '../../../../styles/abstracts/colors';
-import  AppHeader  from '../../../Common/AppHeader';
+import * as Colors from "../../../../styles/abstracts/colors";
+import AppHeader from "../../../Common/AppHeader";
 
-function Support ({navigation}) {
+function Support({navigation}) {
   return (
     <View style={styles.container}>
-       <StatusBar backgroundColor={Colors.primary.color} />
-       <AppHeader navigation={navigation} title="My Support" />
-      <Text style={styles.text}>Support Screen</Text>
-     
-   
+      <StatusBar backgroundColor={Colors.primary.color} />
+      <AppHeader navigation={navigation} title="Support" showBackButton={true} />
+      <Image
+        style={styles.logo2}
+        source={{
+          uri: "https://cdn-icons-png.flaticon.com/512/3135/3135596.png",
+        }}
+      />
+      <TouchableOpacity style={styles.button2} onPress={() => onSubmit()}>
+        <Text style={styles.btnTxt}>Contact Admin</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
-
+  logo2: {
+    position: "absolute",
+    top: 150,
+    left: 75,
+    width: 250,
+    height: 250,
+  },
+  button2: {
+    width: "50%",
+    alignSelf: "center",
+    margin: 20,
+    elevation: 8,
+    backgroundColor: Colors.primary.color,
+    marginTop: 20,
+    top: 400,
+    alignItems: "center",
+    paddingVertical: 0,
+    borderRadius: 30,
+    // minHeight: 100,
+  },
+  btnTxt: {
+    color: "#ffff",
+    padding: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
 
 export default Support;
